1edbb0bf53bec42135c08cece975075456b7e30a
vene23 <152407483+vene23@users.noreply.github.com>
2024-10-29 21:26:27 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,14 +10,6 @@ module.exports = {
   plugins: [],
 };
 
-import React from "react";
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <meta
-      key="viewport"
-      name="viewport"
-      content="width=device-width, initial-scale=1.0"
     />,
   ]);
 };
